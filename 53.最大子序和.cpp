a4b374b829a08369e24037790ec2951334b636f9
liz0527 <17863957587@163.com>
2019-03-12 17:41:03 +0800

    update

---
+++ 53.最大子序和.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 0)
+            return 0;
+        int ans = nums[0], temp = nums[0];
+        for(int i = 1; i < len; i++) {
+            if(temp > 0) {
+                temp = temp + nums[i];
+            } else {
+                temp = nums[i];
+            }
+            ans = max(ans, temp);
+        }
+        return ans;
+    }
+};
